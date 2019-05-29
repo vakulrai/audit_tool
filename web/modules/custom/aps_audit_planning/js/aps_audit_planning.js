a301e3bcc37f66138d8db9f2bce6306db548ee6a
vakulrai42 <vakul.r@opensenselabs.com>
2019-05-29 16:05:44 +0530

    Audit tool : audit planning and tour config error

--- web/modules/custom/aps_audit_planning/js/aps_audit_planning.js
+++ web/modules/custom/aps_audit_planning/js/aps_audit_planning.js
@@ -12,96 +12,48 @@
         right: 'dayGridMonth,timeGridWeek,timeGridDay'
       },
       defaultDate: '2019-04-12',
-      navLinks: true, // can click day/week names to navigate views
+      navLinks: true,
       selectable: true,
       selectMirror: true,
-      // events: {
-      //   url: base_url+'/get-events',
-      //   failure: function() {
-      //   }
-      // },
+      events: {
+        url: base_url+'/generate-events',
+        failure: function() {
+        }
+      },
       select: function(arg) {
+        console.log(arg);
         var title = prompt('Enter A Title:');
         if (title) {
+          var current = formatDate(arg.start);
+          var start = formatDate(arg.start);
+          var end = formatDate(arg.end);
+          var url = base_url + '/node/add/planned_events?field_start_date='+start+'&field_end_date='+end+'&current='+current+'&title='+title;
           calendar.addEvent({
             title: title,
             start: arg.start,
-            url: 'http://google.com/',
+            url: url,
             end: arg.end,
             allDay: arg.allDay,
             editable: false,
           })
-          $.ajax({
-            type: "post",
-            url: base_url + "/generate-events",
-            async: false,
-            data: {field_start_date: arg.start, field_end_date: arg.start, title: title},
-          success: function (result) {
-          }
-        });
         }
         // calendar.unselect()
       },
-      eventLimit: true, // allow "more" link when too many events
-      // events: [
-      //   {
-      //     title: 'All Day Event',
-      //     start: '2019-04-01'
-      //   },
-      //   {
-      //     title: 'Long Event',
-      //     start: '2019-04-07',
-      //     end: '2019-04-10'
-      //   },
-      //   {
-      //     groupId: 999,
-      //     title: 'Repeating Event',
-      //     start: '2019-04-09T16:00:00'
-      //   },
-      //   {
-      //     groupId: 999,
-      //     title: 'Repeating Event',
-      //     start: '2019-04-16T16:00:00'
-      //   },
-      //   {
-      //     title: 'Conference',
-      //     start: '2019-04-11',
-      //     end: '2019-04-13'
-      //   },
-      //   {
-      //     title: 'Meeting',
-      //     start: '2019-04-12T10:30:00',
-      //     end: '2019-04-12T12:30:00'
-      //   },
-      //   {
-      //     title: 'Lunch',
-      //     start: '2019-04-12T12:00:00'
-      //   },
-      //   {
-      //     title: 'Meeting',
-      //     start: '2019-04-12T14:30:00'
-      //   },
-      //   {
-      //     title: 'Happy Hour',
-      //     start: '2019-04-12T17:30:00'
-      //   },
-      //   {
-      //     title: 'Dinner',
-      //     start: '2019-04-12T20:00:00'
-      //   },
-      //   {
-      //     title: 'Birthday Party',
-      //     start: '2019-04-13T07:00:00'
-      //   },
-      //   {
-      //     title: 'Click for Google',
-      //     url: 'http://google.com/',
-      //     start: '2019-04-28',
-      //     editable: true
-      //   }
-      // ]
+      eventLimit: true,
     });
       calendar.render();
+
+      function formatDate(date) {
+        var d = new Date(date),
+            month = '' + (d.getMonth() + 1),
+            day = '' + d.getDate(),
+            year = d.getFullYear();
+
+        if (month.length < 2) month = '0' + month;
+        if (day.length < 2) day = '0' + day;
+
+        return [year, month, day].join('-');
+      }
     }
   };
 }(jQuery));
--- web/modules/custom/aps_audit_planning/src/Controller/AuditPlanningController.php
+++ web/modules/custom/aps_audit_planning/src/Controller/AuditPlanningController.php
@@ -25,10 +25,20 @@ class AuditPlanningController extends ControllerBase {
   }
 
    public function generateEvents(Request $request) {
-    if (isset($_REQUEST)) {
+    $query = \Drupal::database()->select('node_field_data', 'n');
+    $query->join('node__field_start_date', 'st', 'n.nid = st.entity_id');
+    $query->join('node__field_end_date', 'ed', 'st.entity_id = ed.entity_id');
+    $query->fields('n',['title']);
+    $query->fields('st',['field_start_date_value']);
+    $query->fields('ed',['field_end_date_value']);
+    $query->condition('n.type', 'planned_events');
+    $records = $query->execute()->fetchAll();
+    foreach ($records as $key => $value) {
+      $list['title'] = $value->title;
+      $list['start'] = date('Y-m-d', $value->field_start_date_value);
+      $list['end'] = date('Y-m-d', $value->field_end_date_value);
     }
-
-    return new JsonResponse($_REQUEST);
+    return new JsonResponse($list);
   }
 
 }
