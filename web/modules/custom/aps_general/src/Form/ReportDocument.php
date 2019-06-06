2a7b5e7230dfdd8937700f7ef371785dd2325b2e
vakulrai42 <vakul.r@opensenselabs.com>
2019-06-06 11:08:56 +0530

    Audit tool: Added code and cofigurations for pre audit flow

--- web/modules/custom/aps_general/src/Form/ReportDocument.php
+++ web/modules/custom/aps_general/src/Form/ReportDocument.php
@@ -4,6 +4,11 @@ namespace Drupal\aps_general\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\node\Entity\Node;
+use Drupal\paragraphs\Entity\Paragraph;
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\OpenModalDialogCommand;
+use Drupal\Core\Ajax\RemoveCommand;
 
 /**
  * Class ReportDocument.
@@ -22,7 +27,6 @@ class ReportDocument extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $nid = \Drupal::request()->query->get('id');
   	$form['customer_audit'] = [
       '#type' => 'fieldset',
       '#title' => 'Report',
@@ -48,6 +52,16 @@ class ReportDocument extends FormBase {
     $form['actions']['cancel'] = [
       '#type' => 'submit',
       '#value' => $this->t('Cancel'),
+      '#button_type' => 'primary',
+      '#attributes' => [
+        'class' => [
+          'use-ajax',
+        ],
+      ],
+      '#ajax' => [
+        'callback' => [$this, 'CancelUpdateReport'],
+        'event' => 'click',
+      ],
     ];
 
     $form['actions']['submit'] = [
@@ -91,7 +105,39 @@ class ReportDocument extends FormBase {
    * AJAX Callback to update report.
    */
   public function UpdateReport(array $form, FormStateInterface $form_state) {
+    $response = new AjaxResponse();
+    if($nid = \Drupal::request()->query->get('id')){
+      $form_values = $form_state->getValues();
+      $node_object = Node::load($nid);
+      $paragraph = Paragraph::create([
+        'field_reason' => $form_values['reasons'],
+        'field_others' => $form_values['reasons_other'],
+        'type' => 'report_reason',
+      ]);
+      $paragraph->save();
 
+      $paragraphp_version = [
+        [
+          'target_id' => $paragraph->id(),
+          'target_revision_id' => $paragraph->getRevisionId(),
+        ],
+      ];
+
+      $node_object->set('field_reasons_data', $paragraphp_version);
+      $node_object->save();
+    }
+    $response->addCommand(new OpenModalDialogCommand("Success!", 'Report Submitted Successfully.', ['width' => 800]));
+    return $response;
   }
+
+   /**
+   * AJAX Callback to update report.
+   */
+  public function CancelUpdateReport(array $form, FormStateInterface $form_state) {
+    $response = new AjaxResponse();
+    $response->addCommand(new RemoveCommand('.ui-dialog'));
+    return $response;
+  }
+
 
 }
