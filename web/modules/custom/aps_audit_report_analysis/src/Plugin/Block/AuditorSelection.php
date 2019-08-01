3c5dbf22ae27f98fb5ee5e0e44c2be5ed0422b72
vakulrai42 <vakul.r@opensenselabs.com>
2019-08-01 11:56:26 +0530

    Audit tool: Auditor selection report changes

--- web/modules/custom/aps_audit_report_analysis/src/Plugin/Block/AuditorSelection.php
+++ web/modules/custom/aps_audit_report_analysis/src/Plugin/Block/AuditorSelection.php
@@ -39,20 +39,26 @@ class AuditorSelection extends BlockBase {
 	      $first_last_date_monthly[$month_name]['count_underscore_auditor'] = count(getAuditOPtions('auditor_selection','/auditor-and-audit-export/'.$uri[1].'?created[min]='.$first_last_date_monthly[$month_name]['first_day'].'&created[max]='.$first_last_date_monthly[$month_name]['last_day'].'&field_score_value=6'));
 	      $first_last_date_monthly[$month_name]['audit_count'] = count(getAuditOPtions('auditor_selection','/auditor-and-audit-export/'.$uri[1].'?created[min]='.$first_last_date_monthly[$month_name]['first_day'].'&created[max]='.$first_last_date_monthly[$month_name]['last_day']));
 
-	      $user_count += count(getAuditOPtions('auditor_selection','/auditor-and-audit-export/'.$uri[1].'?created[min]='.$first_last_date_monthly[$month_name]['first_day'].'&created[max]='.$first_last_date_monthly[$month_name]['last_day']));
 	      $selected_user_count += $first_last_date_monthly[$month_name]['count_underscore_auditor'] = count(getAuditOPtions('auditor_selection','/auditor-and-audit-export/'.$uri[1].'?created[min]='.$first_last_date_monthly[$month_name]['first_day'].'&created[max]='.$first_last_date_monthly[$month_name]['last_day'].'&field_score_value=6'));
 
-	      $first_last_date_monthly['total_user'] = $user_count;
+	      // $first_last_date_monthly['total_user'] = $user_count;
 	      $first_last_date_monthly['selected_user_count'] = $selected_user_count;
 	      $plot_data['data'][] = [$first_last_date_monthly[$month_name]['count_underscore_auditor']];
 	    }
     }
+    $query = \Drupal::database()->select('user__field_reference_id', 'n');
+    $query->fields('n', ['field_reference_id_target_id','entity_id']);
+    $query->condition('n.bundle', 'user');
+    $query->condition('n.field_reference_id_target_id', $uri[1]);
+    $users = $query->execute()->fetchAll();
+    $user_count = count($users);
+
     $build['audit_auditor_report']['auditor_report'] = [
       '#type' => 'fieldset',
     ];
     $build['audit_auditor_report']['auditor_report'] ['#attached']['library'][] = 'aps_audit_report_analysis/aps_dashboard_auditor_report_js';
     $build['audit_auditor_report']['auditor_report']['#attached']['drupalSettings']['auditor_data'] = json_encode($plot_data['data']);
-    $build['audit_auditor_report']['auditor_report']['#attached']['drupalSettings']['total_user'] = $first_last_date_monthly['total_user'];
+    $build['audit_auditor_report']['auditor_report']['#attached']['drupalSettings']['total_user'] = $user_count;
     $build['audit_auditor_report']['auditor_report']['#attached']['drupalSettings']['selected_user_count'] = $first_last_date_monthly['selected_user_count'];
     $build['audit_auditor_report']['auditor_report']['container_element_audit_auditor_report']['#markup'] = '<div id="container-element-auditor-report" style="min-width: 310px; height: 400px; max-width: 600px; margin: 0 auto"></div>';
 
