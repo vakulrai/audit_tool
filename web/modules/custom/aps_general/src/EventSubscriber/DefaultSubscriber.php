22b96aad1074ac596275a51465709f1ed4bba70c
vakulrai42 <vakul.r@opensenselabs.com>
2019-08-23 19:27:40 +0530

    AT-13: Auditor Page Issues

---
+++ web/modules/custom/aps_general/src/EventSubscriber/DefaultSubscriber.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Drupal\aps_general\EventSubscriber;
+
+use Drupal\Core\Config\ConfigFactory;
+use Drupal\Core\Url;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Drupal\Core\Session\AccountProxy;
+use Drupal\user\Entity\User;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
+/**
+ * RoleBasedUserRedirect event subscriber.
+ *
+ * @package Drupal\aps_general\DefaultSubscriber
+ */
+class DefaultSubscriber implements EventSubscriberInterface {
+
+  /**
+   * The config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactory
+   */
+  protected $configFactory;
+
+   /**
+   * @var Drupal\Core\Session\AccountProxy $account
+   */
+  protected $account;
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(ConfigFactory $configFactory, AccountProxy $account) {
+    $this->configFactory = $configFactory;
+    $this->account = $account;
+  }
+
+  /**
+   * Check redirection.
+   *
+   * @param \Symfony\Component\HttpKernel\Event\FilterResponseEvent $event
+   *   Event.
+   */
+  public function checkRedirection(FilterResponseEvent $event) {
+    global $base_url;
+    $current_path = \Drupal::service('path.current')->getPath();
+    $profile = [];
+
+    if (!$uid) {
+      $uid = $this->account->id();
+    }
+    $user = User::load($uid);
+    $current_user = \Drupal::currentUser();
+    $roles = $current_user->getRoles();
+    foreach ($roles as $key => $value) {
+      $user_role = $value;
+    }
+    $user_unit = $user->field_reference_id->target_id;
+    $front = \Drupal::service('path.matcher')->isFrontPage();
+    
+    if ($front && $user_role == 'auditor') {
+      $response = new RedirectResponse(URL::fromUserInput('/planned-audit-listing/'.$user_unit)->toString());  
+      $response->send(); 
+    }
+    elseif ($front && $user_role == 'auditee') {
+      $response = new RedirectResponse(URL::fromUserInput('/planned-audit-listing-auditee/'.$user_unit)->toString());  
+      $response->send(); 
+    }
+    
+    // //Homepage Redirects for Consumer role.
+    // $valid_consumer_roles = 'consumer';
+    // if ($current_path == '/homepage' && $profile['roles'] == 'consumer') {
+    //   $response = new RedirectResponse($base_url.'/consumer/dashboard');
+    //   $response->send();
+    // }
+  }
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[KernelEvents::RESPONSE][] = ['checkRedirection'];
+    return $events;
+  }
+
+}
+
