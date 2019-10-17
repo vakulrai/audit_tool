08ce9c5b68b0923032f4866f028964ddcfe9e3f1
anmolgoyal74 <anmol.g@opensenselabs.com>
2019-10-17 13:40:49 +0530

    AT-00: Updated Unit address field value

--- web/modules/custom/aps_general/src/Plugin/views/field/AggregateUnitAddress.php
+++ web/modules/custom/aps_general/src/Plugin/views/field/AggregateUnitAddress.php
@@ -3,10 +3,8 @@
 namespace Drupal\aps_general\Plugin\views\field;
 
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Component\Utility\Random;
 use Drupal\views\Plugin\views\field\FieldPluginBase;
 use Drupal\views\ResultRow;
-use Drupal\Core\Url;
 
 /**
  * A handler to provide a field that is completely custom by the administrator.
@@ -53,17 +51,15 @@ class AggregateUnitAddress extends FieldPluginBase {
    */
   public function render(ResultRow $values) {
     $node = $values->_entity;
-    if(count($node->field_remote_address->getValue()) > 0){
+    $options[$node->field_address->locality] = $node->field_address->locality;
+    if (count($node->field_remote_address->getValue()) > 0) {
       foreach ($node->field_remote_address->getValue() as $key => $value) {
-        $support_locations[] = $value['locality'];
+        $options[$value['locality']] = $value['locality'];
       }
-	    $field_address[] = $node->field_address->locality;
-	    $options = array_merge($field_address , $support_locations);
-	    foreach ($options as $adrss_key => $adrss_value) {
-	      $combined_address .= '<b>'.$adrss_value.'</b><br>';
-	    }	
     }
-
+    foreach ($options as $adrss_key => $adrss_value) {
+      $combined_address .= '<b>' . $adrss_value . '</b><br>';
+    }
     $form['unit_address'] = [
       '#type' => 'markup',
       '#markup' => $combined_address,
